a06045f5f2af86d15bfa6c55da5171a0328152a0
Pranish97 <99318459+Pranish97@users.noreply.github.com>
2025-06-25 22:22:14 +0545

    Add token store slice and implement user details management

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,12 @@ const router = require("./routes");
 var cookieParser = require("cookie-parser");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
