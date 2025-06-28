60cb03def993b9a7f9194e5d4295890621f0983c
Pranish97 <99318459+Pranish97@users.noreply.github.com>
2025-06-28 23:04:24 +0545

    Create Admin Panel and get All users

---
+++ backend/controller/allUsers.js
@@ -0,0 +1,24 @@
+const userModel = require("../models/userModel");
+
+async function allUsers(req, res) {
+  try {
+    console.log("userId all users", req.userId);
+
+    const allUser = await userModel.find();
+
+    res.json({
+      message: "All User",
+      data: allUser,
+      success: true,
+      error: false,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message || error,
+      error: true,
+      succes: false,
+    });
+  }
+}
+
+module.exports = allUsers;
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema(
     },
     password: String,
     profileImg: String,
+    role: String,
   },
   {
     timestamps: true,
